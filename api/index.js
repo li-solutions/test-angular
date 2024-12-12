7e80702850150dbe99baa7152c2872c05e6c7ee3
akhoroshailo <>
2024-12-12 22:21:01 +0200

    chore: add vercel config

--- api/index.js
+++ api/index.js
@@ -1,3 +1,3 @@
-const server = import("../dist/test-angular/server/server.mjs");
+import * as server from "../dist/test-angular/server/server.mjs";
 
-module.exports = server.app;
+export default server.app();
